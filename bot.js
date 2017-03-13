19f80a58ee0155f53814049f312396aa60068d33
am1aproject <4am1aproject@gmail.com>
2017-03-13 12:20:22 +0100

    update

--- bot.js
+++ bot.js
@@ -5,10 +5,24 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+
 client.on('message', message => {
   if (message.content === 'ping') {
-    message.reply('pong');
+    message.reply('pong `' + client.ping + 'ms `');
   }
+});
+
+client.on('message', message => {
+  if (message.content === 'whoami') {
+    message.reply("<:windef:290805340998729729>");
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'what is my avatar') {
+    message.reply(message.author.avatarURL);
+  }
+
 });
 
 client.login('MjkwNzc2NTgyNzUzMzUzNzI4.C6f2UA.kbsddY-LTqfEgHJHzW79b4PllLU');
